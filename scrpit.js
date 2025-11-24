6f588e8c140f648ac8c072952a992cc53186c302
Hexterty46 <efsuslusemih@gmail.com>
2025-11-24 14:05:23 +0100

    Input fixs
    de email heeft geen border meer.
    de email send knop svg verbeterd
    bij de ai chat border raius aangepast
    ai chat button hover en focus gezet

--- scrpit.js
+++ scrpit.js
@@ -1,10 +1,10 @@
- // checkbox ENTER key functie
- document.querySelectorAll('input[type="checkbox"]').forEach(cb => {
-  cb.addEventListener('keydown', e => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      cb.checked = !cb.checked;
-      cb.dispatchEvent(new Event("change"));
-    }
-  });
-});
+//  // checkbox ENTER key functie
+//  document.querySelectorAll('input[type="checkbox"]').forEach(cb => {
+//   cb.addEventListener('keydown', e => {
+//     if (e.key === "Enter") {
+//       e.preventDefault();
+//       cb.checked = !cb.checked;
+//       cb.dispatchEvent(new Event("change"));
+//     }
+//   });
+// });
